b593164f564205ddcf61fcb76c235bf8960c4279
nelson gonçalves <nafergo@gmail.com>
2020-03-26 19:29:44 +0000

    Add files via upload

---
+++ images/13408934789347895hewiruh348957/data.js
@@ -0,0 +1,739 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada",
+      "name": "Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -0.6024549495378722,
+        "pitch": 0.0410983006432879,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1705518324812978,
+          "pitch": 0.09898411457096223,
+          "rotation": 4.71238898038469,
+          "target": "1-sala-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9869343472992194,
+          "pitch": -0.2766584981625062,
+          "title": "Ficha Técnica<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-sala-1",
+      "name": "Sala 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.0006977912380410345,
+        "pitch": 0.1430279624539832,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2160206150583477,
+          "pitch": 0.02685663267002525,
+          "rotation": 1.5707963267948966,
+          "target": "0-entrada"
+        },
+        {
+          "yaw": -2.3325847376413194,
+          "pitch": 0.00340766297522066,
+          "rotation": 4.71238898038469,
+          "target": "2-sala-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.846610744003824,
+          "pitch": 0.05045315179066279,
+          "title": "<div>As Publicações</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.815816894429622,
+          "pitch": -0.5327460643793476,
+          "title": "Fotos",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.232507609535756,
+          "pitch": -0.5560153246363626,
+          "title": "<div>Os Cadernos</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.4210092811909707,
+          "pitch": -0.23333974615479036,
+          "title": "Fotos",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.139067043316599,
+          "pitch": 0.4508642734526713,
+          "title": "Info",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.41447347293590475,
+          "pitch": 0.7332480205805645,
+          "title": "Info",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.7977327411967838,
+          "pitch": 0.5928229241689955,
+          "title": "Info",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-sala-2",
+      "name": "Sala 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -0.1691626813471494,
+        "pitch": 0.016865978434841722,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.368768057296437,
+          "pitch": 0.05333979155954793,
+          "rotation": 1.5707963267948966,
+          "target": "6-sala-3"
+        },
+        {
+          "yaw": -1.0925923813389584,
+          "pitch": 0.5927410671735736,
+          "rotation": 0,
+          "target": "3-pedralta"
+        },
+        {
+          "yaw": -0.220633887983384,
+          "pitch": 0.8336173810544487,
+          "rotation": 0,
+          "target": "4-mamaltar"
+        },
+        {
+          "yaw": 0.7354389615117043,
+          "pitch": 0.6497341275817341,
+          "rotation": 0,
+          "target": "5-paranho"
+        },
+        {
+          "yaw": -2.9788556142407963,
+          "pitch": 0.0759753065988118,
+          "rotation": 4.71238898038469,
+          "target": "1-sala-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1430898102405367,
+          "pitch": -0.4289112476398209,
+          "title": "<div>Info</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.4639359559532217,
+          "pitch": -0.5106278796255381,
+          "title": "Info",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.4273802434795506,
+          "pitch": 0.8388084187426923,
+          "title": "Info",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.02379495693795,
+          "pitch": -0.3473834374693574,
+          "title": "Info",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-pedralta",
+      "name": "Pedralta",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6621378309004573,
+          "pitch": 0.024500701308465267,
+          "rotation": 1.5707963267948966,
+          "target": "6-sala-3"
+        },
+        {
+          "yaw": 2.441752627866446,
+          "pitch": 0.6804723969252997,
+          "rotation": 6.283185307179586,
+          "target": "2-sala-2"
+        },
+        {
+          "yaw": 1.5626798351661462,
+          "pitch": 0.4288561357827767,
+          "rotation": 4.71238898038469,
+          "target": "5-paranho"
+        },
+        {
+          "yaw": 1.3295619743693514,
+          "pitch": 0.7016211194306425,
+          "rotation": 4.71238898038469,
+          "target": "4-mamaltar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9022988802779555,
+          "pitch": 0.4060492398800868,
+          "title": "Info",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.14189363199798422,
+          "pitch": 0.6952612488654495,
+          "title": "Info",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-mamaltar",
+      "name": "Mamaltar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4052891119855637,
+          "pitch": 0.6874116025571588,
+          "rotation": 7.0685834705770345,
+          "target": "3-pedralta"
+        },
+        {
+          "yaw": -2.961933806386341,
+          "pitch": 0.8878595623569776,
+          "rotation": 0,
+          "target": "2-sala-2"
+        },
+        {
+          "yaw": 1.1682554019204368,
+          "pitch": 0.7667367064751147,
+          "rotation": 4.71238898038469,
+          "target": "5-paranho"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9643301087177605,
+          "pitch": -0.2570846315706774,
+          "title": "Info",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.2568551151451448,
+          "pitch": -0.10950526032939223,
+          "title": "Info<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6436563605170313,
+          "pitch": 0.3424348571500282,
+          "title": "Info",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-paranho",
+      "name": "Paranho",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.11680560527169881,
+        "pitch": 0.1330538298748607,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3483469102781598,
+          "pitch": 0.8225071059807334,
+          "rotation": 13.351768777756625,
+          "target": "4-mamaltar"
+        },
+        {
+          "yaw": -1.566511675919184,
+          "pitch": 0.43767673297923615,
+          "rotation": 7.0685834705770345,
+          "target": "3-pedralta"
+        },
+        {
+          "yaw": -2.2509219472344206,
+          "pitch": 0.6440866565695096,
+          "rotation": 4.71238898038469,
+          "target": "2-sala-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9830734856793946,
+          "pitch": -0.2963766202361029,
+          "title": "info",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.6890375642608788,
+          "pitch": 0.15719223422358652,
+          "title": "info",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6123556579764049,
+          "pitch": 0.4168425275162235,
+          "title": "Info",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-sala-3",
+      "name": "Sala 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.12643038393659545,
+        "pitch": 0.016865978434841722,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7374777016643073,
+          "pitch": 0.4410830208784713,
+          "rotation": 6.283185307179586,
+          "target": "7-paleoltico"
+        },
+        {
+          "yaw": 0.12122120335754438,
+          "pitch": 0.6756352848011851,
+          "rotation": 0,
+          "target": "8-bronze"
+        },
+        {
+          "yaw": -1.6357009790366597,
+          "pitch": 0.5756832078661187,
+          "rotation": 0,
+          "target": "9-imbrex"
+        },
+        {
+          "yaw": -2.2667452458502684,
+          "pitch": -0.042902464114714434,
+          "rotation": 4.71238898038469,
+          "target": "10-final"
+        },
+        {
+          "yaw": 2.0076315102095386,
+          "pitch": 0.06027810163387848,
+          "rotation": 4.71238898038469,
+          "target": "2-sala-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.241283444464008,
+          "pitch": -0.3796550041915019,
+          "title": "Info",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-paleoltico",
+      "name": "Paleolítico",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.1396263401595519,
+        "pitch": 0.1649117891406764,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.44507393943063,
+          "pitch": 0.6573798907122423,
+          "rotation": 1.5707963267948966,
+          "target": "8-bronze"
+        },
+        {
+          "yaw": -2.1628448390722657,
+          "pitch": 0.5605252929419233,
+          "rotation": 0,
+          "target": "6-sala-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.0145491015350503,
+          "pitch": 0.8557977671582684,
+          "title": "Info",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.5532713341135462,
+          "pitch": 0.013933404000820104,
+          "title": "info",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "8-bronze",
+      "name": "Bronze",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.20004158349781953,
+        "pitch": 0.2983769466717341,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5526007562624828,
+          "pitch": 0.6741007295140342,
+          "rotation": 4.71238898038469,
+          "target": "7-paleoltico"
+        },
+        {
+          "yaw": -2.621823278434592,
+          "pitch": 0.7325651718515136,
+          "rotation": 0,
+          "target": "6-sala-3"
+        },
+        {
+          "yaw": -1.9161291536541185,
+          "pitch": 0.48983709629695227,
+          "rotation": 0,
+          "target": "9-imbrex"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5144200307338611,
+          "pitch": 0.0971923218666415,
+          "title": "Info",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "9-imbrex",
+      "name": "Imbrex",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1525655475506102,
+          "pitch": 0.07498326806888755,
+          "rotation": 4.71238898038469,
+          "target": "10-final"
+        },
+        {
+          "yaw": -2.761485953028256,
+          "pitch": 0.5939665500947946,
+          "rotation": 0,
+          "target": "6-sala-3"
+        },
+        {
+          "yaw": 2.8217803001976236,
+          "pitch": 0.5215208682775323,
+          "rotation": 11.780972450961727,
+          "target": "8-bronze"
+        },
+        {
+          "yaw": 2.9627449371206556,
+          "pitch": 0.3032612731187676,
+          "rotation": 4.71238898038469,
+          "target": "7-paleoltico"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7170271996763518,
+          "pitch": -0.36335181173988573,
+          "title": "Info",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.30241875784077266,
+          "pitch": -0.23264371480871127,
+          "title": "Info",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.4843183803224793,
+          "pitch": 0.28993382205503515,
+          "title": "Info",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "10-final",
+      "name": "Final",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.11008999897193661,
+        "pitch": 0.013117983227099117,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.06796830158288891,
+          "pitch": -0.07320473043424869,
+          "title": "info",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.60696236698376,
+          "pitch": -0.37550270590526047,
+          "title": "Ficha Técnica<br>",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "PAV - Coleção Arqueológica José Coelho ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
