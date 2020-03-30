ee4b448bab66f392d9356bf695afcaf6b303180e
nafergo <nafergo@gmail.com>
2020-03-30 20:28:23 +0100

    0

---
+++ images/epr3408wiorhjw49057u34905/js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.collapsible').collapsible();    
+    $('.tooltipped').tooltip({delay: 50});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
