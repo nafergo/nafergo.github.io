ee62042f209cbd1b3443e21486f53400f670b5ae
nafergo <nafergo@gmail.com>
2020-03-31 19:25:39 +0100

    0

--- images/epr3408wiorhjw49057u34905/js/init.js
+++ images/epr3408wiorhjw49057u34905/js/init.js
@@ -3,8 +3,13 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    $('.collapsible').collapsible();    
-    $('.tooltipped').tooltip({delay: 50});
+    $('.collapsible').collapsible();
+    $('.tooltipped').tooltip();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, options);
+});
