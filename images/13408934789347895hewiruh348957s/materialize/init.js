04a1fb15b7ededf441987bea41639acead18c18a
nafergo <nafergo@gmail.com>
2020-04-07 19:39:15 +0100

    0

---
+++ images/13408934789347895hewiruh348957s/materialize/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.modal').modal();
+    $('.tooltipped').tooltip();
+    $('.collapsible').collapsible();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
