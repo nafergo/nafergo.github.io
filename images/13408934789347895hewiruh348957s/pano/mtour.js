a6c39867f129fc878d5c7f1c867fefd3b1a3a538
nafergo <nafergo@gmail.com>
2020-04-08 23:20:16 +0100

    0

---
+++ images/13408934789347895hewiruh348957s/pano/mtour.js
@@ -0,0 +1,48 @@
+var APP_DATA = {
+    "initialscene": "R0010461",
+    "name": "360",
+    "scenes": [
+        {
+            "faceSize": 3360,
+            "id": "R0010461",
+            "infoHotspots": [
+            ],
+            "initialViewParameters": {
+              "pitch": 0,
+              "yaw": 0,
+              "fov": 1.1707963267948966
+            },
+            "levels": [
+                {
+                    "size": 256,
+                    "tileSize": 256
+                },
+                {
+                    "size": 256,
+                    "tileSize": 256
+                },
+                {
+                    "size": 512,
+                    "tileSize": 256
+                },
+                {
+                    "size": 1024,
+                    "tileSize": 256
+                },
+                {
+                    "size": 2048,
+                    "tileSize": 256
+                }
+            ],
+            "linkHotspots": [
+            ],
+            "name": "R00ss10461"
+        }
+    ],
+    "settings": {
+        "autorotateEnabled": true,
+        "fullscreenButton": false,
+        "mouseViewMode": "drag",
+        "viewControlButtons": false
+    }
+}
