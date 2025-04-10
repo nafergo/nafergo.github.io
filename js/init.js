34b22a79b0fdc1c515fc46ed563334edf6e371d0
nelson gonçalves <nafergo@gmail.com>
2025-04-10 15:08:11 +0100

    1

--- js/init.js
+++ js/init.js
@@ -5,6 +5,12 @@
     $('.tooltipped').tooltip();
     $('.collapsible').collapsible();
     $('.tabs').tabs();
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
